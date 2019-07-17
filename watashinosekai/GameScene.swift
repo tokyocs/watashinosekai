06f4069e00c98e0c920363efbf9677ac7cb0546e
Maya Yatabe <tcs17079@tokyocs.org>
2019-07-17 16:08:04 +0900

    Initial Commit

---
+++ watashinosekai/GameScene.swift
@@ -0,0 +1,51 @@
+//
+//  GameScene.swift
+//  watashinosekai
+//
+//  Created by tcs17079 on 2019/07/16.
+//  Copyright © 2019 tcs17079. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+        // Get label node from scene and store it for use later
+       
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+     
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+    }
+}
