255af7340d67b0f107238ef2aac864745b055581
Waka Fukushima <tcs15057@tokyocs.org>
2019-07-18 14:53:38 +0900

    画面が出た時に音楽が流れるようにした。

--- watashinosekai/GameScene.swift
+++ watashinosekai/GameScene.swift
@@ -8,11 +8,52 @@
 
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
-class GameScene: SKScene {
+
+class GameScene: SKScene,AVAudioPlayerDelegate {
+    
+    var audioPlayer: AVAudioPlayer!
+    func playSound(name: String) {
+        guard let path = Bundle.main.path(forResource: name, ofType: "mp3") else {
+            print("音源ファイルが見つかりません")
+            return
+        }
+        
+        do {
+            // AVAudioPlayerのインスタンス化
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            
+            // AVAudioPlayerのデリゲートをセット
+            audioPlayer.delegate = self
+
+            // 音声の再生
+            audioPlayer.play()
+        } catch {
+        }
+    }
+    func playBGM(name: String) {
+        guard let path = Bundle.main.path(forResource: name, ofType: "mp3") else {
+            print("音源ファイルが見つかりません")
+            return
+        }
+        
+        do {
+            // AVAudioPlayerのインスタンス化
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            
+            // AVAudioPlayerのデリゲートをセット
+            audioPlayer.delegate = self
+            audioPlayer.numberOfLoops = -1
+            
+            // 音声の再生
+            audioPlayer.play()
+        } catch {
+        }
+    }
     
     override func didMove(to view: SKView) {
-        
+      playBGM(name: "Summer_Beach_Ambience")
         // Get label node from scene and store it for use later
        
     }
