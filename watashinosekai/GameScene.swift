7e18b1c43dffeb9ea6c8f0d597bd9997ca6e5808
Maya Yatabe <tcs17079@tokyocs.org>
2019-10-09 11:08:23 +0900

    動けるようにしたと思われる ということです 多分だから信用しないでね

--- watashinosekai/GameScene.swift
+++ watashinosekai/GameScene.swift
@@ -19,6 +19,7 @@ class GameScene: SKScene,AVAudioPlayerDelegate {
     var sitabotnn:SKSpriteNode!
     var migibotann:SKSpriteNode!
     var hidaribotann:SKSpriteNode!
+    var mannnakamati:SKSpriteNode!
     func playSound(name: String) {
         guard let path = Bundle.main.path(forResource: name, ofType: "mp3") else {
             print("音源ファイルが見つかりません")
@@ -61,7 +62,7 @@ class GameScene: SKScene,AVAudioPlayerDelegate {
     
         self.hero = SKSpriteNode(imageNamed: "syuototko")
         self.hero.position = CGPoint(x:0,y:-200)
-        self.hero.zPosition = 1
+        self.hero.zPosition = 2
         self.hero.xScale = 0.09
         self.hero.yScale = 0.09
         addChild(self.hero)
@@ -75,7 +76,7 @@ class GameScene: SKScene,AVAudioPlayerDelegate {
         
         self.syuonna = SKSpriteNode(imageNamed: "syuonna")
         self.syuonna.position = CGPoint(x:12,y:-198)
-        self.syuonna.zPosition = 2
+        self.syuonna.zPosition = 1
         self.syuonna.xScale = 0.07
         self.syuonna.yScale = 0.07
         addChild(self.syuonna)
@@ -108,6 +109,13 @@ class GameScene: SKScene,AVAudioPlayerDelegate {
         self.hidaribotann.yScale = 0.3
         addChild(self.hidaribotann)
         
+        self.mannnakamati = SKSpriteNode(imageNamed:"mannnakamati")
+        self.mannnakamati.position = CGPoint(x:0,y:0)
+        self.mannnakamati.zPosition = 1
+        self.mannnakamati.xScale = 0.235
+        self.mannnakamati.yScale = 0.175
+        addChild(self.mannnakamati)
+        
       playBGM(name: "Summer_Beach_Ambience")
         // Get label node from scene and store it for use later
        
@@ -134,6 +142,35 @@ class GameScene: SKScene,AVAudioPlayerDelegate {
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch: AnyObject in touches {
+            let location = touch.location(in:self)
+            let touchNode = self.atPoint(location)
+            if touchNode == migibotann{
+                let moveToRight = SKAction.moveTo(x:self.hero.position.x + 30,duration:0.2)
+                hero.run(moveToRight)
+                
+            }else if touchNode == hidaribotann {
+                let moveToLeft = SKAction.moveTo(x: self.hero.position.x - 30, duration: 0.2)
+                hero.run(moveToLeft)
+            }else if touchNode == sitabotnn{
+                // 下に動く動きを指定する。
+                let moveToUp = SKAction.moveTo(y: self.hero.position.y - 30, duration: 0.2)
+                // 下に動かす。
+                hero.run(moveToUp)
+                // 上のボタンが押されたら上に30px動かす。
+            }else if touchNode == uebotann {
+                // 上に動く動きを指定する。
+                let moveToUp = SKAction.moveTo(y: self.hero.position.y + 30, duration: 0.2)
+                // 上に動かす。
+                hero.run(moveToUp)
+
+                
+                
+                
+                
+            }
+            
+        }
      
     }
     
